34c5002df1536f0b2daa13d221e8f5219c4bb577
Stellmaria <aam.cheshire@gmail.com>
2026-08-19 16:33:27 +0300

    fix: make rose formatting deterministic

--- src/main/java/com/it_academy/flower_shop/entity/model/flower/Rose.java
+++ src/main/java/com/it_academy/flower_shop/entity/model/flower/Rose.java
@@ -1,7 +1,9 @@
 package com.it_academy.flower_shop.entity.model.flower;
 
 import lombok.EqualsAndHashCode;
-import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
+import java.util.Objects;
 
 import static java.lang.String.format;
 
@@ -9,18 +11,18 @@ import static java.lang.String.format;
  * @author Anastasia Melnikova
  */
 @EqualsAndHashCode(callSuper = true)
-@NotNull
 public class Rose extends Flower {
     private final String thorn;
 
     public Rose(String name, int freshness, double length, double price, String thorn) {
         super(name, freshness, length, price);
-        this.thorn = thorn;
+        this.thorn = Objects.requireNonNull(thorn, "thorn must not be null");
     }
 
     @Override
     public String toString() {
-        return format("Name: %s | " + "Freshness: %d | " + "length: %.2f | " + "Price: %.2f | " + "Thorn: %s",
+        return format(Locale.ROOT,
+                "Name: %s | Freshness: %d | length: %.2f | Price: %.2f | Thorn: %s",
                 getName(), getFreshness(), getLength(), getPrice(), thorn);
     }
 }
